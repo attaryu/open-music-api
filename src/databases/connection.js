c62e3c7eb8ece6544c05d01385ff601f4f3f5a73
attaryu <mattarannaufal@gmail.com>
2025-07-22 17:10:51 +0700

    fix: update environment variables in .env.example and configure database connection in connection.js

--- src/databases/connection.js
+++ src/databases/connection.js
@@ -1,5 +1,11 @@
 const { Pool } = require('pg');
 
-const db = new Pool();
+const db = new Pool({
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+});
 
 module.exports = db;
